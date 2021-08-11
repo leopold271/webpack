8ad80e6a5d48159f00c8a1ec0c9a625269b0ae4c
leopold271 <lenya.m1998@gmail.com>
2021-08-11 15:40:50 +0300

    webpack demo

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.css"
+
+const App = () => {
+    return(
+        <div className="app">
+            react is working
+        </div>
+    );
+};
+
+export default App;
